6f4ced0e3cc4e6f76b53c440aeae82307d72ffc2
TheAlepo <sanchezc.alexe@gmail.com>
2026-05-14 11:52:51 -0400

    main policy model skeleton

--- reinforce/model.py
+++ reinforce/model.py
@@ -3,11 +3,18 @@ import jax.numpy as np
 import flax.nnx as nnx
 
 class PolicyModel(nnx.Module):
-    def __init__(self , rngs):
-        self.model = nnx.Sequential(
-            nnx.Linear(1 , 32 , rngs=rngs),
-            nnx.Linear(32 , 64 , rngs=rngs),
-        )
-    
+    def __init__(self , input_size , hidden_size , output_size , * , rngs):
+        self.fc1 = nnx.Linear(input_size , hidden_size , rngs=rngs)
+        self.fc2 = nnx.Linear(hidden_size , output_size , rngs=rngs)
+
     def __call__(self , x):
-        return self.model(x)
+        x = self.fc1(x)
+        x = nnx.relu(x)
+        logits = self.fc2(x)
+
+        probs = jax.nn.softmax(logits)
+        action = jax.random.categorical(rngs.params() , logits)
+
+        log_probs = jax.nn.log_softmax(logits)
+
+        return
