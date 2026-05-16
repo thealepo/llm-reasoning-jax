90a6a1fc89a9401f32c75a939950683fd6b238a3
TheAlepo <sanchezc.alexe@gmail.com>
2026-05-16 19:33:27 -0400

    set the hyperparameters as global variables.

--- reinforce/train.py
+++ reinforce/train.py
@@ -5,17 +5,24 @@ import optax
 import gymnasium as gym
 from model import PolicyModel
 
+# Hyperparameters
+NUM_EPISODES = 1_000
+GAMMA = 0.99
+LEARNING_RATE = 1e-3
+
+# Setting the JAX keys and Environment (CartPole-v1 for this Implementation)
 key = jax.random.PRNGKey(42)
 env = gym.make('CartPole-v1')
 state , _ = env.reset()
 
+# Initializing the custom policy model, and optimizer
 model = PolicyModel(
     input_size=state.shape[0],
     hidden_size=128,
     output_size=env.action_space.n,
     rngs=nnx.Rngs(42)
 )
-optimizer = nnx.Optimizer(model , optax.adam(1e-3) , wrt=nnx.Param)
+optimizer = nnx.Optimizer(model , optax.adam(LEARNING_RATE) , wrt=nnx.Param)
 
 def loss_fn(model , states , actions , returns):
     returns = (returns - returns.mean()) / (returns.std() + 1e-8)
@@ -29,7 +36,7 @@ def train_step(model , optimizer , states , actions , returns):
     grads = nnx.grad(loss_fn)(model , states , actions , returns)
     optimizer.update(model , grads)
 
-for episode in range(10_000):
+for episode in range(NUM_EPISODES):
     # Generate one episode
     episode_data = []
     state , _ = env.reset()
@@ -52,10 +59,8 @@ for episode in range(10_000):
     # Calculate the return G
     returns = []
     G = 0
-    gamma = 0.99
-
     for _ , _ , reward in reversed(episode_data):
-        G = reward + gamma * G
+        G = reward + GAMMA * G
         returns.append(G)
     returns = returns[::-1]
 
