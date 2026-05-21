d6d1471b93d05ec7f4fd21528b597f2b800359a5
TheAlepo <sanchezc.alexe@gmail.com>
2026-05-21 14:29:17 -0400

    finetune.py more-or-less completed

--- star/finetune.py
+++ star/finetune.py
@@ -3,10 +3,29 @@ import json
 import optax
 from gemma import gm
 from kauldron import kd
+from datasets import Dataset
+
+def examples_to_hf_dataset(examples):
+    records = {
+        'prompt': [],
+        'response': []
+    }
+
+    for question , answer , reasoning in examples:
+        records['prompt'].append(question)
+        records['response'].append(f'{reasoning}\n#### {answer}')
+
+    return Dataset.from_dict(records) 
 
 def finetune(model , params , examples , tokenizer , workdir , num_steps=300 , batch_size=8 , lr=1e-3):
     os.makedirs(workdir , exist_ok=True)
 
+    ckpt_dir = os.path.join(workdir , 'init_ckpt')
+    gm.ckpts.save_params(params , ckpt_dir)
+
+    hf_ds = examples_to_hf_dataset(examples)
+
+    # Building the new dataset for Kauldron finetuning
     ds = kd.data.py.HuggingFace(
         dataset='''some data''',
         batch_size=batch_size,
@@ -23,3 +42,26 @@ def finetune(model , params , examples , tokenizer , workdir , num_steps=300 , b
             ),
         ],
     )
+
+    # Loss
+    loss = kd.losses.SoftmaxCrossEntropyWithIntLabels(
+        logits='preds.logits',
+        labels='batch.target',
+        mask='batch.loss_mask'
+    )
+
+    # Trainer!
+    trainer = kd.train.Trainer(
+        seed=42,
+        workdir=os.path.join(workdir , 'trainer_out'),
+        train_ds=ds,
+        model=model,
+        init_transform=gm.ckpts.LoadCheckpoint(path=ckpt_dir),
+        num_train_steps=num_steps,
+        train_loss={'loss': loss},
+        optimizer=optax.adam(learning_rate=lr),
+    )
+
+    state , _ = trainer.train()
+
+    return state.params
