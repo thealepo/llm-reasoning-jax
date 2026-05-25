ab58e1d91e6e05d164d00856b08c564659adca67
TheAlepo <sanchezc.alexe@gmail.com>
2026-05-25 12:24:32 -0400

    spacing

--- star/prompts.py
+++ star/prompts.py
@@ -3,13 +3,13 @@ Given the question, think step-by-step for the answer.
 '''
 
 FEW_SHOT_EXAMPLES = '''\
-Q: Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?
-A: Natalia sold 48/2 = <<48/2=24>>24 clips in May.
+Q:Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?
+A:Natalia sold 48/2 = <<48/2=24>>24 clips in May.
 Natalia sold 48+24 = <<48+24=72>>72 clips altogether in April and May.
 #### 72
 
-Q: Weng earns $12 an hour for babysitting. Yesterday, she just did 50 minutes of babysitting. How much did she earn?
-A: Weng earns 12/60 = $<<12/60=0.2>>0.2 per minute.
+Q:Weng earns $12 an hour for babysitting. Yesterday, she just did 50 minutes of babysitting. How much did she earn?
+A:Weng earns 12/60 = $<<12/60=0.2>>0.2 per minute.
 Working 50 minutes, she earned 0.2 x 50 = $<<0.2*50=10>>10.
 #### 10
 '''
