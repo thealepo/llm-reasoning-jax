fixed loss error... it was outputting batch_size, but averageing it out could give us the loss of the batch... much better :)

ppo/rlhf/train/train_rm.py
@@ -104,7 +104,7 @@ class Transformer(nnx.Module):
 # (input_ids_winner , input_ids_loser , mask_winner , mask_loser)
 
 def bradley_terry_loss(y_winner , y_loser):
-    return -jnp.log(jax.nn.sigmoid(y_winner - y_loser))
+    return -jnp.log(jax.nn.sigmoid(y_winner - y_loser)).mean()
 
 def train_step(state_model , state_optimizer , input_ids_winner , input_ids_loser , mask_winner , mask_loser):
     model = nnx.merge(graphdef_rm , state_model)