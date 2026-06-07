68d5fd470a32a99f6b4763ad057bbcd31ed308be
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-07 11:10:22 -0400

    new JIT-able autoregressive generation intent

--- ppo/rlhf/models/policy.py
+++ ppo/rlhf/models/policy.py
@@ -52,19 +52,50 @@ class PolicyModel(nnx.Module):
 
         return buffer
 
+    # attempt at JIT-able autoregressiv egeneration
+    def generate(self , prompt , rng , max_new_tokens=MAX_NEW_TOKENS):
+        batch , prompt_len = prompt.shape
+        total_len = prompt_len + max_new_tokens
+
+        # Setting a vuffer of size [batch , total_len]
+        buffer = jnp.zeros((batch,total_len) , dtype=jnp.int32)
+        buffer = buffer.at[: , :prompt_len].set(prompt)  # first part is prompt, second part is zeros ment to be filled.
+
+        # Functioning to check if we are over the max_new_tokens
+        def cond_fn(carry):
+            i , _ , _ = carry
+            return i < max_new_tokens
+
+        def body_fn(carry):
+            i , buffer , rng = carry
+            rng , rng_sample = jax.random.split(rng)
+
+            logits = self(buffer) # [batch , total_len , vocab]
+            next_token = jax.random.categorical(
+                rng_sample , logits[: , prompt_len + i - 1 , :] , axis=1
+            ) # [batch]
+            buffer = buffer.at[: , prompt_len + i].set(next_token)
+            return i+1 , buffer , rng
+
+        _ , buffer , _ = jax.while_loop(
+            cond_fn , body_fn , (jnp.int32(0) , buffer , rng)
+        )
+        return buffer
+
 if __name__ == "__main__":
     config = TransformerConfig()
     policy = PolicyModel(config , rngs=nnx.Rngs(0))
-
-    input_ids = jnp.ones((4,32) , dtype=jnp.int32)
-    log_probs = policy.log_probs_of(input_ids)
-
-    assert log_probs.shape == (4,32) , f"Expected (4 , 32) but got {log_probs.shape}"
-    assert jnp.all(log_probs <= 0) , f"Log Probs must be negative"
-    print("Yay!")
-
-    # generate() method tests@
     rng = jax.random.PRNGKey(42)
-    prompt = jnp.ones((4,32) , dtype=jnp.int32)
-    output = policy.generate(prompt , rng=rng)
-    print(output.shape) # (4 , 288)
+
+    BATCH , PROMPT_LEN , MAX_TOKENS = 2 , 4 , 16
+    prompt = jax.random.randint(rng , (BATCH,PROMPT_LEN) , 1 , config.VOCAB_SIZE)
+
+    output = policy.generate(prompt , rng=rng , max_new_tokens=MAX_TOKENS)
+
+    assert output.shape == (BATCH , PROMPT_LEN + MAX_TOKENS) , f"Bad shape: {output.shape}"
+    print("checkmark on shape")
+
+    assert jnp.array_equal(output[: , :PROMPT_LEN] , prompt)
+    print("pass prompt")
+
+    
