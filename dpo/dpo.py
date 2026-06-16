47e6fadc311335eefed3f149bf875aa8c555b320
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-16 16:07:59 -0400

    full DPO loss func

--- dpo/dpo.py
+++ dpo/dpo.py
@@ -2,14 +2,27 @@
 # optimize policy model to minimize the DPO_LOSS, for the given reference model, dataset, and KL Beta.
 
 def dpo_loss(x , policy , reference ,y_winner , y_loser , ):
-    # Log prob sampling
-    logits_pw = policy(y_winner)
-    log_probs_pw = jax.nn.log_softmax(logits_pw , axis=-1)
-    token_log_probs_policy_winner = jnp.take_along_axis(
-        log_probs_pw , y_winner[... , jnp.newaxis] , axis=2
-    ).squeeze(-1)
+    # per token log provs per model
+    def get_token_log_probs(model , y):
+        logits = model(y)
+        log_probs = jax.nn.log_softmax(logits , axis=-1)
+        token_log_probs = jnp.take_along_axis(
+            log_probs , y[... , jnp.newaxis] , axis=2
+        ).squeeze(-1)
+        return token_log_probs.sum(axis=-1)
 
+    policy_log_probs_winner = get_token_log_probs(policy , y_winner)
+    policy_log_probs_loser = get_token_log_probs(policy , y_loser)
 
+    reference_log_probs_winner = get_token_log_probs(reference , y_winner)
+    reference_log_probs_loser = get_token_log_probs(reference , y_loser)
+
+    # DPO ratios
+    log_ratio_winner = policy_log_probs_winner - reference_log_probs_winner
+    log_ratio_loser = policy_log_probs_loser - reference_log_probs_loser
 
     # Loss
-    return -jnp.mean(jnp.log(jax.nn.sigmoid(BETA*jnp.exp(token_log_probs_policy_winner - token_log_probs_reference_winner)-BETA*(token_log_probs_policy_loser - token_log_probs_reference_loser))))
+    logits_dpo = BETA * (log_ratio_winner - log_ratio_loser)
+    loss = -jnp.mean(jax.nn.log_sigmoid(logits_dpo))
+
+    return loss
