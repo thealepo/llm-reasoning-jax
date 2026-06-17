c179b3cdf934558bd2c353064b1793de478e72aa
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 10:35:16 -0400

    training plan for DPO

---
+++ dpo/train.py
@@ -0,0 +1,4 @@
+# pass in the data (x , y_winner , y_loser)
+#   here there is still quesitons, but i presume concat x,ys and then feed the models the slice
+# compute the loss
+# update the policy & policy optimizer
