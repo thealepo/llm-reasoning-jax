f9e8368585f36e386b95e84a1364be8dfdd7c26b
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 10:37:22 -0400

    train_step

--- dpo/train.py
+++ dpo/train.py
@@ -2,3 +2,10 @@
 #   here there is still quesitons, but i presume concat x,ys and then feed the models the slice
 # compute the loss
 # update the policy & policy optimizer
+
+def train_step():
+    def loss_fn():
+        return dpo_loss(policy , reference , y_winner , y_loser)
+
+    pass
+
