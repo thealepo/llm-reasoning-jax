9aff6de5e61043c226e6f0767a9b6733cd765ac5
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 10:26:57 -0400

    hyperparam

--- dpo/dpo.py
+++ dpo/dpo.py
@@ -4,6 +4,8 @@ import jax
 import jax.numpy as jnp
 from flax import nnx
 
+BETA = 0.95
+
 def dpo_loss(x , policy , reference ,y_winner , y_loser , ):
     # per token log provs per model
     def get_token_log_probs(model , y):
