a8594369b1fce685093b620c050ecbd6aba17fa1
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 17:01:53 -0400

    corrected the get_token_log_probs to include the given prompt (x)

--- dpo/dpo.py
+++ dpo/dpo.py
@@ -1,24 +1,36 @@
 # we sample two reponses given a prompt, from our reference SFT model
 # optimize policy model to minimize the DPO_LOSS, for the given reference model, dataset, and KL Beta.
+from pprint import pp
 import jax
 import jax.numpy as jnp
 from flax import nnx
 
 BETA = 0.95
 
-def dpo_loss(x , policy , reference , y_winner , y_loser , ):
+def dpo_loss(policy , reference , x , y_winner , y_loser , ):
     # per token log provs per model
     def get_token_log_probs(model , y):
-        logits = model(y)
+        full_seq = jnp.concatenate([x,y] , axis=-1)
+        logits = model(full_seq)
         log_probs = jax.nn.log_softmax(logits , axis=-1)
+
+        targets = full_seq[: , 1:]
+        log_probs = log_probs[: , :-1 , :]
+
         token_log_probs = jnp.take_along_axis(
-            log_probs , y[... , jnp.newaxis] , axis=2
+            log_probs , targets[... , jnp.newaxis] , axis=-1
         ).squeeze(-1)
-        return token_log_probs.sum(axis=-1)
+
+        # mask stuff
+        prompt_len = x.shape[1]
+        completion_len = y.shape[1]
+        mask = jnp.arange(token_log_probs.shape[1])
+        mask = (mask >- prompt_len-1) & (mask < prompt_len-1 + completion_len)
+        return (token_log_probs * mask).sum(axis=-1)
+
 
     policy_log_probs_winner = get_token_log_probs(policy , y_winner)
     policy_log_probs_loser = get_token_log_probs(policy , y_loser)
-
     reference_log_probs_winner = get_token_log_probs(reference , y_winner)
     reference_log_probs_loser = get_token_log_probs(reference , y_loser)
 
