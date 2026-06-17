6523da9fce502926953e447430d125c5a92feafc
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 16:15:52 -0400

    value and grad

--- dpo/train.py
+++ dpo/train.py
@@ -2,13 +2,19 @@
 #   here there is still quesitons, but i presume concat x,ys and then feed the models the slice
 # compute the loss
 # update the policy & policy optimizer
+import jax
+import jax.numpy as jnp
+from flax import nnx
+from dpo import dpo_loss
 
-def train_step():
-    def loss_fn():
-        return dpo_loss(policy , reference , y_winner , y_loser)
+def train_step(policy , reference , optimizer , batch):
+    x , y_winner , y_loser = batch
 
+    def loss_fn(x , policy , reference , y_winner , y_loser):
+        return dpo_loss(x , policy , reference , y_winner , y_loser)
     grad_fn = nnx.value_and_grad(loss_fn)
-    loss , grads = grad_fn(policy , reference , y_winner , y_loser)
+    
+    loss , grads = grad_fn(x , policy , reference , y_winner , y_loser)
     optimizer.update(policy , grads)
     return loss
 
