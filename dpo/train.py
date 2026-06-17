0013c258883da68c5b4517b03036ba0e92125668
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-17 16:05:22 -0400

    training strucutre

--- dpo/train.py
+++ dpo/train.py
@@ -7,5 +7,8 @@ def train_step():
     def loss_fn():
         return dpo_loss(policy , reference , y_winner , y_loser)
 
-    pass
+    grad_fn = nnx.value_and_grad(loss_fn)
+    loss , grads = grad_fn(policy , reference , y_winner , y_loser)
+    optimizer.update(policy , grads)
+    return loss
 
