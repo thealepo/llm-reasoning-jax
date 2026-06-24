1b8d35b5aca5ffc22e272abe01ed2f4e352228e8
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-24 19:03:54 -0400

    DPO fully done

--- dpo/train.py
+++ dpo/train.py
@@ -5,17 +5,16 @@
 import jax
 import jax.numpy as jnp
 from flax import nnx
-from dpo import dpo_loss
+from .dpo import dpo_loss
 
 @nnx.jit
 def train_step(policy , reference , optimizer , batch):
     x , y_winner , y_loser = batch
 
-    def loss_fn(x , policy , reference , y_winner , y_loser):
+    def loss_fn(policy):
         return dpo_loss(x , policy , reference , y_winner , y_loser)
-    grad_fn = nnx.value_and_grad(loss_fn)(policy)
     
-    loss , grads = grad_fn(x , policy , reference , y_winner , y_loser)
+    loss , grads = nnx.value_and_grad(loss_fn)(policy)
     optimizer.update(policy , grads)
     return loss
 
@@ -28,3 +27,32 @@ def train_epoch(policy , reference , optimizer , data):
 
     return losses
 
+
+if __name__ == "__main__":
+    import optax
+    from dpo.policy import PolicyModel
+    from dpo.transformer import TransformerConfig
+
+    # rngs
+    rng = jax.random.PRNGKey(42)
+    config = TransformerConfig()
+
+    # models and optimizer
+    policy = PolicyModel(config , rngs=nnx.Rngs(0))
+    reference = PolicyModel(config , rngs=nnx.Rngs(0))
+    optimizer = nnx.Optimizer(policy , optax.adam(1e-3) , wrt=nnx.Param)
+
+    # data
+    BATCH , PROMPT_LEN , RESPONSE_LEN = 2 , 8 , 8
+
+    rng_x , rng_winner , rng_loser = jax.random.split(rng , 3)
+    x = jax.random.randint(rng_x , (BATCH,PROMPT_LEN) , 0 , config.VOCAB_SIZE)
+    y_winner = jax.random.randint(rng_winner , (BATCH,RESPONSE_LEN) , 0 , config.VOCAB_SIZE)
+    y_loser = jax.random.randint(rng_loser , (BATCH,RESPONSE_LEN) , 0 , config.VOCAB_SIZE)
+
+    data = [(x , y_winner , y_loser)] * 5
+
+    # training
+    for epoch in range(3):
+        losses = train_epoch(policy , reference , optimizer , data)
+        print(f'epoch {epoch+1} | average loss = {sum(losses)/len(losses):.3f}')
