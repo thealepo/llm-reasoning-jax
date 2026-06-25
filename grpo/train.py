e628c1f436d3434d169362c985c902ec7e5c2476
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-25 00:32:39 -0400

    blet continue

--- grpo/train.py
+++ grpo/train.py
@@ -7,3 +7,11 @@
 
 # for 0 -> mu: update policy by maximizing GRPO objective (???)
 # update reward model through continuos training (???)
+
+#====
+
+# prompt 1 | prompt 2 | prompt 3 | ... | prompt BATCH
+# output 1 output 2 output 3 ... output g | output 1 output 2 output 3 ... output g | ... etc.
+# compute rewards per
+
+# has a vmap or pmap taste to it.
