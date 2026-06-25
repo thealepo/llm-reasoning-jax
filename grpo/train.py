0a9c39ca33863368383742045974e915fea9bbef
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-25 14:17:16 -0400

    generate() function but honeslty i am getting tripped up. will revisit soon.

--- grpo/train.py
+++ grpo/train.py
@@ -14,4 +14,29 @@
 # output 1 output 2 output 3 ... output g | output 1 output 2 output 3 ... output g | ... etc.
 # compute rewards per
 
-# has a vmap or pmap taste to it.
+# has a vmap taste to it.
+import jax
+import jax.numpy as jnp
+from flax import nnx
+
+BETA = 0.01
+MAX_NEW_TOKENS = 32
+MU = 4  # ppo_epochs equivalency
+G = 8
+
+def generate_group(graphdefs , state_policy , input_ids , prompt_len , rng):
+    # Unpacking
+    graphdef_policy = graphdefs[0]
+    B = input_ids.shape[0]  # batch , prompt_len
+
+    # RNG Stuff
+    rng_keys = jax.random.split(rng , B*G).reshape(B , G , 2)
+
+    #
+
+    # Merging
+    policy = nnx.merge(graphdef_policy , state_policy)
+
+    # Generate g ys from the policy, given the input_ids.
+    # return should be [batch , g , prompt_len+max_new_tokens]
+    
